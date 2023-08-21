47cb156b2fe061853e28f7278c0e7b4d91cb209c
noor-sehimi <nourshimi17@gmail.com>
2023-08-22 01:25:59 +0200

    done miss you can check it out

---
+++ layout/script.js
@@ -0,0 +1,13 @@
+ console.log("hello hello");
+ let name = "mehdi"
+ let age = 19
+ name = dhia
+ console.log(name);
+ const nameagain = "mariem"
+ console.log(nameagain);
+ let x= 1.5
+ console.log(x);
+ console.log(x / 2)
+ const array = ["nour","mariem"]
+ const ages = [20, 16, 17]
+ console.log(arr[1])
